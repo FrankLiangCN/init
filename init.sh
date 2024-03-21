906751dd40db538064e4d9f5ad31ba037b172e97
FrankLiangCN <111636925+FrankLiangCN@users.noreply.github.com>
2024-03-22 00:40:37 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,28 +2,19 @@
 
 echo ""
 echo " ========================================================= "
-echo " \              Linux 环境初始化自动部署脚本             / "
+echo " +              Linux 环境初始化自动部署脚本             + "
 echo " ========================================================= "
-echo "\n"
-
-# 迫于经常在新机器上配置各种环境，写了一个初始化安装脚本
-# 此脚本将安装常见工具并配置vim/wget/curl/pip3/zsh等,默认只配置基本环境
-# LEVEL= config/dev/hacker/full
-# config主要就是配置zsh/vim/wget/cur等系统配置,然后安装一些常见的软件例如git htop ncdu vim等
-# dev是安装开发环境java/python3/Go/Node/Docker
-# hacker 安装常见的网络安全工具，例如sqlmap/nmap/httpx/subfinder/ffuf/xray等
-# full 一把梭
-
-LEVEL='full'
+echo ""
 
 
 # 开启SSH密钥登录选项
 # 检测PubkeyAuthentication参数
+echo "正在设置SSH密钥登录选项"
 pubkey_auth=$(sed -n 's/^PubkeyAuthentication\s*\(.*\)/\1/p' /etc/ssh/sshd_config)
 
 # 如果PubkeyAuthentication参数为空值
 if [ -z "$pubkey_auth" ]; then
-  echo "PubkeyAuthentication参数为空值，将添加PubkeyAuthentication yes 和 RSAAuthentication yes参数"
+  echo "PubkeyAuthentication参数为空值，将添加 PubkeyAuthentication yes 和 RSAAuthentication yes 参数"
   echo "PubkeyAuthentication yes" >> /etc/ssh/sshd_config
   echo "RSAAuthentication yes" >> /etc/ssh/sshd_config
   echo "SSH密钥登录选项已开启"
@@ -34,7 +25,7 @@ elif [ "$pubkey_auth" = "no" ]; then
   echo "SSH密钥登录选项已开启"
 # 如果PubkeyAuthentication参数为yes
 elif [ "$pubkey_auth" = "yes" ]; then
-  echo "PubkeyAuthentication参数已为yes，将添加RSAAuthentication yes参数"
+  echo "PubkeyAuthentication参数已为yes，将添加 RSAAuthentication yes 参数"
   echo "RSAAuthentication yes" >> /etc/ssh/sshd_config
   echo "SSH密钥登录选项已开启"
 fi
@@ -44,16 +35,18 @@ service sshd restart
 
 # 设置系统时区
 timedatectl set-timezone Asia/Hong_Kong
-echo "设置系统时区为 Asia/Hong_Kong成功！"
+echo "设置系统时区为 Asia/Hong_Kong 成功！"
 
 # apt 更新
-echo "apt update ..."
+echo "apt updating ..."
 apt update >/dev/null 2>&1
+echo "apt 已更新"
 
 # 检测curl是否已安装
 if ! type curl &>/dev/null; then
   echo "curl 未安装，正在安装..."
   apt install curl -y
+  echo "curl 已安装"
 else
   echo "curl 已安装"
 fi
@@ -62,6 +55,7 @@ fi
 if ! type wget &>/dev/null; then
   echo "wget 未安装，正在安装..."
   apt install wget -y
+  echo "wget 已安装"
 else
   echo "wget 已安装"
 fi
@@ -70,6 +64,7 @@ fi
 if ! type unzip &>/dev/null; then
   echo "unzip 未安装，正在安装..."
   apt install unzip -y
+  echo "unzip 已安装"
 else
   echo "unzip 已安装"
 fi
@@ -78,6 +73,7 @@ fi
 if ! type vim &>/dev/null; then
   echo "vim 未安装，正在安装..."
   apt install unzip -y
+  echo "vim 已安装"
 else
   echo "vim 已安装"
 fi
@@ -86,6 +82,7 @@ fi
 if ! type vnstat &>/dev/null; then
   echo "vnstat 未安装，正在安装..."
   apt install vnstat -y
+  echo "vnstat 已安装"
 else
   echo "vnstat 已安装"
 fi
@@ -93,7 +90,8 @@ fi
 # 检测ddns-go是否已安装
 if ! type ddns-go &>/dev/null; then
   echo "ddns-go 未安装，正在安装..."
-  wget -qO- https://raw.githubusercontent.com/FrankLiangCN/DDNS/main/ddns.sh | bash
+  bash <(curl -Ls https://raw.githubusercontent.com/FrankLiangCN/DDNS/main/ddns.sh)
+  echo "ddns-go 已安装"
 else
   echo "ddns-go 已安装"
 fi
@@ -103,7 +101,7 @@ if ! type x-ui &>/dev/null; then
   echo "x-ui 未安装，正在安装..."
   bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
 else
-  echo "ddns-go 已安装"
+  echo "x-ui 已安装"
 fi
 
 echo "Linux 环境初始化自动部署成功！"
