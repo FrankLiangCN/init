8be791e628dc6a8be813856fb708e2ca7ba827eb
FrankLiangCN <111636925+FrankLiangCN@users.noreply.github.com>
2024-04-09 00:03:15 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -224,40 +224,37 @@ else
   echo -e "Docker已安装\n"
 fi
 
+# 检测是否已经安装Docker容器Portainer
 if ! type docker &>/dev/null; then
   echo -e "安装Portainer容器前，需先安装Docker!\n"
   read -p "是否安装Docker？ (y/n)": answer
   if [[ x"$answer" == x"y" || x"$answer" == x"Y" ]]; then
-    echo "开始安装Docker..."
-    # Docker安装指令
-    curl -fsSL https://get.docker.com | bash
+    echo -e "开始安装Docker...\n"
+    curl -fsSL https://get.docker.com | bash || (echo "Docker 安装失败，请重试！" ; exit 1)
+    echo ""
+    if type docker &>/dev/null; then
+      echo -e "进入Portainer安装脚本...\n"
+      if ! docker ps | grep portainer &>/dev/null; then
+        read -p "Portainer未安装，是否安装？ (y/n)": answer
+        if [[ x"$answer" == x"y" || x"$answer" == x"Y" ]]; then
+          echo -e "开始安装Portainer...\n"
+          docker volume create portainer_data
+          docker run -d --network host --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest || (echo "Portainer 安装失败，请重试！" ; exit 1)
+          echo -e "Portainer安装成功，5分钟内访问 http://IP:9000 进行初始化配置\n"
+        else
+          echo -e "取消安装Portainer\n"
+        fi
+      else
+        echo -e "Portainer已安装\n"
+      fi
+    else
+      echo -e "Docker未安装，退出Portainer容器安装!\n"
+    fi
   else
-	echo -e "Docker未安装成功!\n"
+    echo -e "Docker未安装成功!\n"
   fi
 else
   echo -e "取消安装Docker并退出Portainer容器安装\n"
 fi
-
-# 检测是否已经安装Docker容器Portainer
-if type docker &>/dev/null; then
-  echo -e "进入Portainer安装脚本...\n"
-  if ! docker ps | grep portainer &>/dev/null; then
-    read -p "Portainer未安装，是否安装？ (y/n)": answer
-    if [[ x"$answer" == x"y" || x"$answer" == x"Y" ]]; then
-      echo -e "开始安装Portainer...\n"
-      # Portainer安装指令
-      docker volume create portainer_data
-      docker run -d --network host --name portainer --restart=always -v /var/r
-      echo -e "Portainer安装成功，5分钟内访问 http://IP:9000 进行初始化配置\n"
-    else
-      echo -e "取消安装Portainer\n"
-    fi
-  else
-    echo -e "Portainer已安装\n"
-  fi
-else
-  echo -e "Docker未安装成功，退出Portainer容器安装!\n"
-fi
-
 
 echo -e "Linux 环境初始化自动部署成功！\n"
