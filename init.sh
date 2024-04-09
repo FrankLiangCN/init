9b997a875a959c37cf12feb8d1194e1820bb8866
FrankLiangCN <111636925+FrankLiangCN@users.noreply.github.com>
2024-04-09 22:31:24 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -305,7 +305,7 @@ elif type docker &>/dev/null; then
     if [[ x"$answer" == x"y" || x"$answer" == x"Y" ]]; then
       echo -e "开始安装Watchtower...\n"
       docker run -d --name watchtower --restart=unless-stopped -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower
-      echo -e "Watchtower安装成功，5分钟内访问 http://IP:9000 进行初始化配置\n"
+      echo -e "Watchtower安装成功\n"
     else
       echo -e "Watchtower取消安装\n"
     fi
