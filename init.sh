34da3b90ed0a3fcdbe8a27e1d2db7097c5181677
FrankLiangCN <111636925+FrankLiangCN@users.noreply.github.com>
2024-04-15 01:04:55 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -439,7 +439,7 @@ if ! type /opt/cleandata.sh &>/dev/null; then
     chmod +x /opt/cleandata.sh
     echo -e "${yellow}正在清理磁盘空间${plain}\n"
     bash /opt/cleandata.sh
-    echo -e "${yellow}清理磁盘空间已清理${plain}"
+    echo -e "${yellow}磁盘空间已清理${plain}"
     echo "0 0 */7 * *  bash /opt/cleandata.sh > /dev/null 2>&1" >> /var/spool/cron/crontabs/root
     #echo "0 0 */7 * *  root bash /opt/cleandata.sh > /dev/null 2>&1" >> /etc/crontab
     echo -e "${green}定时清理磁盘空间任务已设置${plain}\n"
