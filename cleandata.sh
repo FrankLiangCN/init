95aed56eb09382ec7a482e9f45d60b038952aa40
FrankLiangCN <111636925+FrankLiangCN@users.noreply.github.com>
2024-04-15 01:01:36 +0800

    Update cleandata.sh

--- cleandata.sh
+++ cleandata.sh
@@ -6,6 +6,10 @@
 # 配置到crontab
 # 0 0 */2 * *  bash /opt/cleandata.sh > /dev/null 2>&1
 
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
 
 # 清理 journal 日志
 # 清理日志到只剩下 10M
@@ -13,13 +17,11 @@
 
 # 清理一天前的日志
 journalctl --vacuum-time=1d
-echo "journal 日志已清理"
-echo ""
+echo -e "${yellow}journal 日志已清理${plain}\n"
 
 # 清理 apt-get 缓存
 apt-get clean
-echo "apt-get 缓存已清理"
-echo ""
+echo -e "${yellow}apt-get 缓存已清理${plain}\n"
 
 #/tmp --设置查找的目录；
 #-mtime +30 --设置修改时间为30天前；
@@ -30,10 +32,8 @@ echo ""
 
 # 清理 7 天前的日志
 find /var/log -mtime +7 -type f -name "*" -exec rm -rf {} \;
-echo "日志已清理"
-echo ""
+echo -e "${yellow}7天前日志已清理${plain}\n"
 
 # 清理 7 天前的临时文件
 find /tmp -mtime +7 -type f -name "*" -exec rm -rf {} \;
-echo "临时文件已清理"
-echo ""
+echo -e "${yellow}7天前临时文件已清理${plain}\n"
