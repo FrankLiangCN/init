b1c553a3d944d32a9c42414e5fd8b877f63dafd2
FrankLiangCN <111636925+FrankLiangCN@users.noreply.github.com>
2024-04-15 01:03:00 +0800

    Update clean_alpine.sh

--- clean_alpine.sh
+++ clean_alpine.sh
@@ -6,11 +6,14 @@
 # 配置到crontab
 # 0 0 */2 * *  bash /opt/clean_alpine.sh > /dev/null 2>&1
 
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
 
 # 清理 apt-get 缓存
 apk cache clean
-echo "apk 缓存已清理"
-echo ""
+echo -e "${yellow}apk 缓存已清理${plain}\n"
 
 
 #/tmp --设置查找的目录；
@@ -22,10 +25,8 @@ echo ""
 
 # 清理 7 天前的日志
 find /var/log -mtime +7 -type f -name "*" -exec rm -rf {} \;
-echo "日志已清理"
-echo ""
+echo -e "${yellow}7天前日志已清理${plain}\n"
 
 # 清理 7 天前的临时文件
 find /tmp -mtime +7 -type f -name "*" -exec rm -rf {} \;
-echo "临时文件已清理"
-echo ""
+echo -e "${yellow}7天前临时文件已清理${plain}\n"
