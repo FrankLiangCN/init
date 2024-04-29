3f18fb6e246af4769a3a0f453bd4559e1d865083
FrankLiangCN <111636925+FrankLiangCN@users.noreply.github.com>
2024-04-30 01:30:00 +0800

    Update init_zsh.zsh

--- init_zsh.zsh
+++ init_zsh.zsh
@@ -86,7 +86,7 @@ if [ -d "$HOME/.oh-my-zsh" ]; then
 
   source ~/.zshrc
 #  echo -e "${Green}.zshrc 配置文件已生效${Plain}"
-  echo -e "${Yellow}稍后需手动运行 ${UBlue}source ~/.zshrc${Yellow} 使新配置生效${Plain}"
+  echo -e "${Yellow}稍后需手动运行 ${UBlue}source ~/.zshrc${Yellow} 使新配置生效${Plain}\n"
 else
   echo -e "oh-my-zsh ${Red}未安装${Plain}"
   echo -e "${Yellow}请参考文档或手动安装: ${UBlue}https://github.com/ohmyzsh/ohmyzsh${Plain}\n"
