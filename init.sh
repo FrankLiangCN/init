e11abd315edf7951572eb0d212bee50fd0c4420f
FrankLiangCN <111636925+FrankLiangCN@users.noreply.github.com>
2024-05-07 02:24:13 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -140,7 +140,7 @@ else
   echo -e "${Red}取消NAT64配置${Plain}\n"
 fi
 
-# 安装ddns-go
+# 安装 ddns-go
 # 定义ddns-go配置文件路径
 ddns_config_file="/etc/systemd/system/ddns-go.service"
 
@@ -151,6 +151,53 @@ ddns_login_info () {
   echo -e "${Green}请访问 ${UBlue}http://IP${ddns_port}${Green} 进行初始化配置${Plain}\n"
 }
 
+# ddns-go 安装
+Install_ddns-go () {
+  if Option; then
+    echo -e "${Yellow}开始安装 ddns-go ...${Plain}"
+    bash <(curl -sSL https://raw.githubusercontent.com/FrankLiangCN/DDNS/main/ddns.sh)
+    if [ $? -eq 0 ]; then
+      echo -e "${Green}ddns-go 安装/更新 成功${Plain}"
+    else
+      echo -e "${Red}ddns-go 安装失败，请参考文档或手动安装: ${UBlue}https://github.com/FrankLiangCN/DDNS${Plain}\n"
+    fi
+  else
+    Cancel_info
+  fi
+}
+
+# 恢复 ddns-go 配置
+Config_ddns() {
+  if type ddns-go &>/dev/null; then
+    read -p "是否恢复 ddns-go 配置？${Default}" answer
+    if Option; then
+      read -p "输入配置来源URL：" source_url
+      if [ -z "$source_url" ]; then
+        source_url=https://sub.vsky.uk/ddns
+      fi
+      echo -e "配置来源URL为：${Yellow}$source_url${Plain}\n"
+      read -p "输入配置来源路径：" path
+      if [ -z "$path" ]; then
+        path=default
+      fi
+      echo -e "配置来源路径为：${Yellow}$path${Plain}\n"
+      echo -e "${Yellow}开始恢复 ddns-go 配置...${Plain}\n"
+      curl -s -o /opt/ddns-go/.ddns_go_config.yaml ${source_url}/${path}/.ddns_go_config.yaml
+      if [[ $? -ne 0 ]]; then
+      	curl -s -o /opt/ddns-go/.ddns_go_config.yaml ${source_url}/${path}/.ddns_go_config.yaml
+      else
+      	echo -e "${Red}ddns-go 配置恢复失败${Plain}\n"
+      fi
+      systemctl daemon-reload
+      systemctl restart ddns-go
+      echo -e "${Green}ddns-go 配置已恢复${Plain}\n"
+    else
+      echo -e "${Yellow}保留 ddns-go 当前配置${Plain}\n"
+    fi
+  fi
+}
+
+# 配置 ddns-go 端口
 Config_ddns_port () {
   echo -e "当前 ddns-go 端口为${Yellow}$ddns_port${Plain}"
   read -p "是否 更新 ddns-go 端口？${Default}" answer
@@ -175,28 +222,16 @@ Config_ddns_port () {
   fi
 }
 
-Install_ddns-go () {
-  if Option; then
-    echo -e "${Yellow}开始安装 ddns-go ...${Plain}"
-    bash <(curl -sSL https://raw.githubusercontent.com/FrankLiangCN/DDNS/main/ddns.sh)
-    if [ $? -eq 0 ]; then
-      echo -e "${Green}ddns-go 安装/更新 成功${Plain}"
-    else
-      echo -e "${Red}ddns-go 安装失败，请参考文档或手动安装: ${UBlue}https://github.com/FrankLiangCN/DDNS${Plain}\n"
-    fi
-  else
-    Cancel_info
-  fi
-}
-
 if ! type ddns-go &>/dev/null; then
   read -p "是否安装 ddns-go？${Default}" answer
   Install_ddns-go
+  Config_ddns
   Config_ddns_port
 else
   echo -e "${Green}ddns-go 已安装${Plain}"
   read -p "是否更新 ddns-go？${Default}" answer
   Install_ddns-go
+  Config_ddns
   Config_ddns_port
 fi
 
