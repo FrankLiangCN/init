952e2421db2d79d0a9f190389336f095e570a5b9
FrankLiangCN <111636925+FrankLiangCN@users.noreply.github.com>
2024-05-11 23:49:37 +0800

    Create caddy.sh

---
+++ caddy.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+Red='\033[0;31m'
+Green='\033[0;32m'
+Yellow='\033[0;33m'
+Blue='\033[0;34m'
+UBlue='\033[4;34m'
+Plain='\033[0m'
+
+# 定义项
+Option() {
+  [[ x"$answer" == x"yes" || x"$answer" == x"YES" || x"$answer" == x"y" || x"$answer" == x"Y" || x"$answer" == x"" ]]
+}
+
+Default='(y/n) [默认yes]:'
+
+Cancel_info() {
+  echo -e "${Red}取消安装${Plain}\n"
+}
+
+Install_succ() {
+  echo -e "${Green}安装成功${Plain}\n"
+}
+
+# 安装Caddy
+if ! type caddy &>/dev/null; then
+  read -p "是否安装 Caddy？${Default}" answer
+  if Option; then
+    echo -e "${Yellow}正在安装 Caddy ...${Plain}"
+    # Caddy安装指令
+    apt install -y debian-keyring debian-archive-keyring apt-transport-https
+    curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+    curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
+    apt update && apt install caddy
+    echo -e "${Green}Caddy 安装成功${Plain}\n"
+  else
+    Cancel_info
+  fi
+else
+  echo -e "${Green}Caddy 已安装${Plain}\n"
+fi
