50d9f29742d6e51afa9bee1b5fe5166f94acde3e
FrankLiangCN <111636925+FrankLiangCN@users.noreply.github.com>
2024-05-20 15:21:46 +0800

    Update ssh_key.sh

--- script/ssh_key.sh
+++ script/ssh_key.sh
@@ -24,12 +24,6 @@ pubkey_authentication=$(grep -E "^\s*PubkeyAuthentication\s+" $ssh_config_file |
 # 检测RSAAuthentication参数
 rsa_authentication=$(grep -E "^\s*RSAAuthentication\s+" $ssh_config_file | awk '{print $2}')
 
-ssh_key_enable() {
-  echo -e "SSH Key 登录选项${Green}已开启${Plain}"
-  service ssh restart
-  echo -e "${Red}SSH 服务已重启${Plain}\n"
-}
-
 # 恢复 SSH Key
 Import_sshkey() {
   if [ "$pubkey_authentication" = "yes" ] && [ "$rsa_authentication" = "yes" ]; then
@@ -60,35 +54,37 @@ Import_sshkey() {
   fi
 }
 
+ssh_key_enable() {
+  echo -e "SSH Key 登录选项${Green}已开启${Plain}"
+  service ssh restart
+  echo -e "${Red}SSH 服务已重启${Plain}\n"
+  Import_sshkey
+}
+
 # 判断SSH参数并修改配置
 if [ "$pubkey_authentication" = "no" ] && [ "$rsa_authentication" = "no" ]; then
   echo -e "${Yellow}修改${Plain} PubkeyAuthentication 和 RSAAuthentication 参数为 ${Green}yes${Plain}..."
   sed -i 's/^\s*PubkeyAuthentication\s*no/PubkeyAuthentication yes/g' $ssh_config_file
   sed -i 's/^\s*RSAAuthentication\s*no/RSAAuthentication yes/g' $ssh_config_file
   ssh_key_enable
-  Import_sshkey
 elif [ "$pubkey_authentication" = "yes" ] && [ "$rsa_authentication" = "no" ]; then
   echo -e "${Yellow}修改${Plain} RSAAuthentication 参数为 ${Green}yes${Plain}..."
   sed -i 's/^\s*RSAAuthentication\s*no/RSAAuthentication yes/g' $ssh_config_file
   ssh_key_enable
-  Import_sshkey
 elif [ "$pubkey_authentication" = "yes" ] && [ -z "$rsa_authentication" ]; then
   echo -e "${Yellow}增加${Plain} RSAAuthentication ${Green}yes${Plain}..."
   echo "RSAAuthentication yes" >> $ssh_config_file
   ssh_key_enable
-  Import_sshkey
 elif [ "$pubkey_authentication" = "no" ] && [ -z "$rsa_authentication" ]; then
   echo -e "${Yellow}修改${Plain} PubkeyAuthentication 参数为 ${Green}yes${Plain} 和 ${Yellow}增加${Plain} RSAAuthentication ${Green}yes${Plain}..."
   sed -i 's/^\s*PubkeyAuthentication\s*no/PubkeyAuthentication yes/g' $ssh_config_file
   echo "RSAAuthentication yes" >> $ssh_config_file
   ssh_key_enable
-  Import_sshkey
 elif [ -z "$pubkey_authentication" ] && [ -z "$rsa_authentication" ]; then
   echo -e "${Yellow}增加${Plain} PubkeyAuthentication ${Green}yes${Plain} 和 RSAAuthentication ${Green}yes${Plain}..."
   echo "PubkeyAuthentication yes" >> $ssh_config_file
   echo "RSAAuthentication yes" >> $ssh_config_file
   ssh_key_enable
-  Import_sshkey
 else
   echo -e "SSH Key 登录选项${Green}已开启${Plain}，无需修改配置...\n"
   Import_sshkey
