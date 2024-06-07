409b3fa97fb48e4ac74759adbdf751b4b66ec6f8
FrankLiangCN <111636925+FrankLiangCN@users.noreply.github.com>
2024-06-08 00:31:57 +0800

    Update clean_docker_log.sh

--- script/clean_docker_log.sh
+++ script/clean_docker_log.sh
@@ -7,11 +7,11 @@ Blue='\033[0;34m'
 UBlue='\033[4;34m'
 Plain='\033[0m'
 
-echo -e "${Green}======== start clean docker containers logs ========${Plain}\n"
+echo -e "${Green}======== Start Cleaning Docker Containers Logs ========${Plain}\n"
 logs=$(find /var/lib/docker/containers/ -name '*-json.log')
 for log in $logs
   do
     echo -e "clean logs : $log\n"
     cat /dev/null > $log
   done
-echo -e "${Yellow}======== end clean docker containers logs ========${Plain}\n"
+echo -e "${Yellow}=========== Docker Containers Logs Cleaned ===========${Plain}\n"
