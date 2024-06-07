f58527f69fe71a51fcc3627758788d15c5a92303
FrankLiangCN <111636925+FrankLiangCN@users.noreply.github.com>
2024-06-08 00:17:38 +0800

    Update cleandata.sh

--- script/cleandata.sh
+++ script/cleandata.sh
@@ -6,10 +6,12 @@
 # 配置到crontab
 # 0 0 */2 * *  bash /opt/cleandata.sh > /dev/null 2>&1
 
-red='\033[0;31m'
-green='\033[0;32m'
-yellow='\033[0;33m'
-plain='\033[0m'
+Red='\033[0;31m'
+Green='\033[0;32m'
+Yellow='\033[0;33m'
+Blue='\033[0;34m'
+UBlue='\033[4;34m'
+Plain='\033[0m'
 
 # 清理 journal 日志
 # 清理日志到只剩下 10M
@@ -17,11 +19,11 @@ plain='\033[0m'
 
 # 清理一天前的日志
 journalctl --vacuum-time=1d
-echo -e "${yellow}journal 日志已清理${plain}\n"
+echo -e "${Yellow}journal 日志已清理${Plain}\n"
 
 # 清理 apt-get 缓存
 apt-get clean
-echo -e "${yellow}apt-get 缓存已清理${plain}\n"
+echo -e "${Yellow}apt-get 缓存已清理${Plain}\n"
 
 #/tmp --设置查找的目录；
 #-mtime +30 --设置修改时间为30天前；
@@ -30,10 +32,10 @@ echo -e "${yellow}apt-get 缓存已清理${plain}\n"
 #-exec rm -rf --查找完毕后执行删除操作；
 # {} \; --固定写法
 
-# 清理 7 天前的日志
-find /var/log -mtime +7 -type f -name "*" -exec rm -rf {} \;
-echo -e "${yellow}7天前日志已清理${plain}\n"
+# 清理 1 天前的日志
+find /var/log -mtime +1 -type f -name "*" -exec rm -rf {} \;
+echo -e "${Yellow}1天前日志已清理${Plain}\n"
 
-# 清理 7 天前的临时文件
-find /tmp -mtime +7 -type f -name "*" -exec rm -rf {} \;
-echo -e "${yellow}7天前临时文件已清理${plain}\n"
+# 清理 1 天前的临时文件
+find /tmp -mtime +1 -type f -name "*" -exec rm -rf {} \;
+echo -e "${Yellow}1天前临时文件已清理${Plain}\n"
