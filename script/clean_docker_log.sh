80bddcd278eec2d3e4c21db741f654c13402a9c0
FrankLiangCN <111636925+FrankLiangCN@users.noreply.github.com>
2024-06-07 21:54:00 +0800

    Update clean_docker_log.sh

--- script/clean_docker_log.sh
+++ script/clean_docker_log.sh
@@ -1,9 +1,9 @@
-#!/bin/sh 
-echo -e "======== start clean docker containers logs ========"  
-logs=$(find /var/lib/docker/containers/ -name '*-json.log')  
-for log in $logs  
-  do  
-    echo -e "clean logs : $log\n"  
-    cat /dev/null > $log  
-  done  
+#!/bin/sh
+echo -e "======== start clean docker containers logs ========"
+logs=$(find /var/lib/docker/containers/ -name '*-json.log')
+for log in $logs
+  do
+    echo -e "clean logs : $log\n"
+    cat /dev/null > $log
+  done
 echo -e "======== end clean docker containers logs ========\n"
