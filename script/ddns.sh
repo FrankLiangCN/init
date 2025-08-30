2e05666ce640a9316f5be8dc0f599b8558574910
FrankLiangCN <111636925+FrankLiangCN@users.noreply.github.com>
2025-08-30 23:17:10 +0800

    Update ddns.sh

--- script/ddns.sh
+++ script/ddns.sh
@@ -48,7 +48,7 @@ Config_ddns() {
     if Option; then
       read -p "输入配置来源URL：" source_url
       if [ -z "${source_url}" ]; then
-        source_url=https://sub.vsky.uk/ddns
+        source_url=https://sub.1980118.xyz/ddns
       fi
       echo -e "配置来源URL为：${Yellow}${source_url}${Plain}\n"
       read -p "输入配置来源路径：" path
